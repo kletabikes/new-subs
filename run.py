231b70c18eb6294c0ad5da43de2520b9a45ac541
kletabikes <info@kleta.com>
2024-09-06 16:55:49 +0200

    primer commit

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import os
 import streamlit as st
 import time
@@ -11,6 +12,15 @@ import pandas as pd
 load_dotenv()
 
 # Configurar las credenciales dependiendo si estamos en local o en Streamlit Cloud
+=======
+import streamlit as st
+import os
+from scripts.kpis import main as kpis_main
+
+st.set_page_config(layout="wide")
+
+# Cargar credenciales desde variables de entorno o desde los secretos de Streamlit
+>>>>>>> 9db4931 (primer commit)
 if "GITHUB_TOKEN" in os.environ:
     GITHUB_USERNAME = os.getenv("GITHUB_USERNAME")
     GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
@@ -22,6 +32,7 @@ else:
     VALID_EMAIL = st.secrets["VALID_EMAIL"]
     VALID_PASSWORD = st.secrets["VALID_PASSWORD"]
 
+<<<<<<< HEAD
 BRANCH_NAME = "main"  # Trabajamos solo con la rama main
 
 def check_goals_exist():
@@ -34,6 +45,10 @@ def check_goals_exist():
             return True
     return False
 
+=======
+
+# Función para la lógica del login
+>>>>>>> 9db4931 (primer commit)
 def login():
     st.title("Iniciar sesión")
 
@@ -43,6 +58,7 @@ def login():
     if st.button("Iniciar sesión"):
         if email == VALID_EMAIL and password == VALID_PASSWORD:
             st.session_state['logged_in'] = True
+<<<<<<< HEAD
             st.rerun()  # Recargar la página después de iniciar sesión
         else:
             st.error("Correo electrónico o contraseña incorrectos.")
@@ -90,6 +106,32 @@ def run_main_loop():
 
     else:
         main()
+=======
+            st.experimental_rerun()  # Recargar la página después de iniciar sesión
+        else:
+            st.error("Correo electrónico o contraseña incorrectos.")
+
+
+# Función principal de la app
+def main():
+
+    # Verificar si el usuario ya ha iniciado sesión
+    if 'logged_in' not in st.session_state:
+        st.session_state['logged_in'] = False
+
+    # Si el usuario no ha iniciado sesión, mostrar pantalla de login
+    if not st.session_state['logged_in']:
+        login()
+    else:
+        # Botón de cerrar sesión
+        if st.sidebar.button("Cerrar sesión"):
+            st.session_state['logged_in'] = False
+            st.experimental_rerun()  # Recargar la página después de cerrar sesión
+
+        # Ejecutar el contenido principal de la aplicación (kpis)
+        kpis_main()
+
+>>>>>>> 9db4931 (primer commit)
 
 if __name__ == "__main__":
     st.set_page_config(layout="wide", initial_sidebar_state="expanded")
