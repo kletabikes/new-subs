caa9f423427315d771041567f2a1004d91308682
kletabikes <info@kleta.com>
2024-09-16 10:44:36 +0200

    m

--- fetch_data/fetch_subscriptions.py
+++ fetch_data/fetch_subscriptions.py
@@ -5,13 +5,8 @@ import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from assets import config
 
-
 def fetch_subscriptions_data():
-<<<<<<< HEAD
     query_string = f"(select 'SUBSCRIPTIONS').{{id: text(SubNinoxID), 'fecha_de_pago': text('Created on'), tipo:text(WIX_MODELOS.Tipo), 'descontar_nuevos': text('Descontar de Nuevos Usuarios'), page:text(Page), status:text(Status)}}"
-=======
-    query_string = f"(select {config.TABLE_NAME}).{{id: text(SubNinoxID), 'fecha_de_inicio': text('Fecha Inicio'), tipo:text(WIX_MODELOS.Tipo), page:text(Page), plan: text(WIX_PLANES.Plan), status:text(Status), ciudad:text(USERS.WIX_CIUDAD.Ciudad)}}"
->>>>>>> 9db4931 (primer commit)
     encoded_query = urllib.parse.quote(query_string)
     query_url = f"{config.NINOX_API_ENDPOINT}teams/{config.NINOX_TEAM_ID}/databases/{config.NINOX_DATABASE_ID}/query?query={encoded_query}"
     headers = {
@@ -26,27 +21,9 @@ def fetch_subscriptions_data():
         
         if records:
             df = pd.json_normalize(records)
-<<<<<<< HEAD
             
             df_filtered = df[df['status'] != 'Not Happened']
             return df_filtered
-=======
-
-            df['ciudad'] = df['ciudad'].apply(lambda x: 'Barcelona' if x.lower() == 'barcelona' 
-                                              else 'Valencia' if x.lower() == 'valencia' 
-                                              else 'otros')
-
-            df = df[df['page'] == 'suscripcion']
-            df = df[df['status'] != 'Not Happened']
-
-            df['fecha_de_inicio'] = pd.to_datetime(df['fecha_de_inicio'], errors='coerce')
-            df[['fecha_de_inicio', 'tipo']] = df[['fecha_de_inicio', 'tipo']].replace('', pd.NA)
-
-            # Eliminar filas con NaN en 'fecha_de_inicio' o 'tipo'
-            df = df.dropna(subset=['fecha_de_inicio', 'tipo'])
-
-            return df
->>>>>>> 9db4931 (primer commit)
         else:
             return pd.DataFrame()
     except requests.exceptions.RequestException as e:
--- scripts/kpis.py
+++ scripts/kpis.py
@@ -1,6 +1,8 @@
-import streamlit as st
+import os
 import pandas as pd
-import plotly.express as px
+import streamlit as st
+from datetime import datetime, timedelta
+from fetch_data.fetch_sales import fetch_sales_data, process_sales_data
 from fetch_data.fetch_subscriptions import fetch_subscriptions_data
 import base64
 
@@ -16,7 +18,6 @@ KLETA_COLORS = {
     'secondary_4': '#033082'
 }
 
-<<<<<<< HEAD
 SUBSCRIPTIONS_CSV = 'data/subscriptions.csv'
 LAST_PROCESSED_CSV = 'data/last_processed.csv'
 GOALS_CSV = 'data/monthly_goals.csv'
@@ -362,32 +363,17 @@ def main():
         # Procesar solo suscripciones que llegaron después de la última fecha procesada
         if last_processed_date is not None:
             new_subs = df_subscriptions[df_subscriptions['fecha_de_pago'] > last_processed_date]
-=======
-def main():
-    st.title("Registro de salidas")
-
-    try:
-        df = fetch_subscriptions_data()
-        if df.empty:
-            st.warning("No se encontraron datos.")
->>>>>>> 9db4931 (primer commit)
         else:
-            st.sidebar.title("Filtros")
+            new_subs = df_subscriptions
 
-            # Definir el rango máximo de fechas disponible en los datos
-            min_date = df['fecha_de_inicio'].min().date()
-            max_date = df['fecha_de_inicio'].max().date()
+        if not new_subs.empty:
+            new_subs_list = new_subs[['id', 'fecha_de_pago', 'tipo', 'descontar_nuevos', 'page']].values.tolist()
+            store_new_subscriptions(new_subs_list)
+            update_last_processed(new_subs['id'].max(), new_subs['fecha_de_pago'].max())
 
-            # Filtro de rango de fechas hasta la última fecha disponible
-            start_date, end_date = st.sidebar.slider(
-                "Selecciona un rango de fechas",
-                min_value=min_date,
-                max_value=max_date,
-                value=(min_date, max_date),
-                format="YYYY-MM-DD"
-            )
+    if 'goals_entered' not in st.session_state or not st.session_state['goals_entered']:
+        st.stop()
 
-<<<<<<< HEAD
     goals = get_goals()
     if goals:
         subscriptions_count = count_subscriptions_by_type()
@@ -396,80 +382,6 @@ def main():
         show_scorecards(subscriptions_count, sales_data, goals)
     else:
         st.warning("No se encontraron objetivos mensuales. Por favor, ingrese los objetivos.")
-=======
-            # Filtro de ciudad
-            ciudades = df['ciudad'].unique().tolist()
-            ciudad_seleccionada = st.sidebar.multiselect(
-                "Ciudad", ciudades, default=ciudades
-            )
-
-            # Filtro de tipo de bicicleta, mostrando solo 'Mecanica' por defecto
-            tipos_bicis = df['tipo'].unique().tolist()
-            tipo_seleccionado = st.sidebar.multiselect(
-                "Selecciona uno o más tipos de bicicletas", tipos_bicis, default=["MECANICA"]
-            )
-
-            # Filtro de tipo de plan (ya no incluye "Todos")
-            planes = df['plan'].unique().tolist()
-            plan_seleccionado = st.sidebar.multiselect(
-                "Selecciona uno o más tipos de plan", planes
-            )
-
-            # Filtro de granularidad de tiempo (día, semana, mes, trimestre)
-            granularidad = st.sidebar.selectbox(
-                "Selecciona la granularidad de los datos", 
-                ["Día", "Semana", "Mes", "Trimestre"]
-            )
-
-            # Si no se ha seleccionado ningún plan, no mostrar gráficos
-            if plan_seleccionado:
-                # Filtrar los datos según el rango de fechas, ciudad, tipo de bici y plan
-                df_filtered = df[
-                    (df['fecha_de_inicio'].dt.date >= start_date) &
-                    (df['fecha_de_inicio'].dt.date <= end_date) &
-                    (df['ciudad'].isin(ciudad_seleccionada)) &
-                    (df['tipo'].isin(tipo_seleccionado)) &
-                    (df['plan'].isin(plan_seleccionado))
-                ]
-
-                # Función para aplicar la granularidad
-                def aplicar_granularidad(df, granularidad):
-                    if granularidad == "Día":
-                        return df['fecha_de_inicio'].dt.date
-                    elif granularidad == "Semana":
-                        return df['fecha_de_inicio'].dt.to_period('W').apply(lambda r: r.start_time)
-                    elif granularidad == "Mes":
-                        return df['fecha_de_inicio'].dt.to_period('M').apply(lambda r: r.start_time)
-                    elif granularidad == "Trimestre":
-                        return df['fecha_de_inicio'].dt.to_period('Q').apply(lambda r: r.start_time)
-
-                # Separar los gráficos por cada plan seleccionado
-                for plan in plan_seleccionado:
-                    st.subheader(f"Registro de salidas de bicicletas para el plan: {plan}")
-
-                    # Filtrar datos por el plan actual
-                    plan_filtered = df_filtered[df_filtered['plan'] == plan]
-
-                    # Aplicar la granularidad
-                    plan_filtered['fecha_agrupada'] = aplicar_granularidad(plan_filtered, granularidad)
-
-                    # Agrupar por la fecha agrupada y contar cuántas bicicletas salieron por intervalo
-                    bikes_per_day = plan_filtered.groupby('fecha_agrupada').size().reset_index(name='count')
-
-                    # Crear el gráfico dinámico con Plotly para este plan
-                    fig = px.line(bikes_per_day, x='fecha_agrupada', y='count',
-                                  title=f'Salidas de bicicletas por {granularidad.lower()} para el plan {plan}',
-                                  labels={'fecha_agrupada': 'Fecha', 'count': 'Cantidad de salidas'},
-                                  template='plotly_white')
-
-                    # Mostrar el gráfico en Streamlit
-                    st.plotly_chart(fig, use_container_width=True)
-            else:
-                st.warning("Por favor selecciona al menos un plan para ver los gráficos.")
-
-    except ConnectionError as e:
-        st.error(f"Error al obtener los datos: {e}")
->>>>>>> 9db4931 (primer commit)
 
 if __name__ == "__main__":
     main()
